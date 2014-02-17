7eb871f71a7e8178624b11bafb7c8f0da4607fce
Christopher Lillthors <lillt@kth.se>
2014-02-17 01:56:39 +0100

    Fixing...

--- Binary_tree/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-javac -cp .:/afs/nada.kth.se/home/n/u1qfoj5n/Private/Programmering/Java/Junit/junit-4.11.jar:/afs/nada.kth.se/home/n/u1qfoj5n/Private/Programmering/Java/Junit/hamcrest-core-1.3.jar *java
-java -cp .:/afs/nada.kth.se/home/n/u1qfoj5n/Private/Programmering/Java/Junit/junit-4.11.jar:/afs/nada.kth.se/home/n/u1qfoj5n/Private/Programmering/Java/Junit/hamcrest-core-1.3.jar org.junit.runner.JUnitCore TestBinaryTree
--- Quicksort/Quicksort.java
+++ Quicksort/Quicksort.java
@@ -1,8 +1,9 @@
 public class Quicksort {
 	
 	private static class Partition {
-		public Partition(int A[],int p,int r) {
-			int x = A[r];
+		public static int doPartition(int A[],int p,int r) {
+			int x,i;
+			x = A[r];
 			i = p - 1;
 			for (int j = p; j <= r - 1; j++) {
 				if (A[j] <= x) {
@@ -14,7 +15,7 @@ public class Quicksort {
 		return i+1;
 		}
 
-		private void swap(int[] array, int a, int b) {
+		private static void swap(int[] array, int a, int b) {
 			int a0 = array[a];
 			array[a] = array[b];
 			array[b] = a0; 
@@ -25,6 +26,19 @@ public class Quicksort {
 		//do nothing
 	}
 
-	public void sort() {
+	public static void main(String[] args) {
+		Quicksort sorting = new Quicksort();
+		int buffer[] = {3,3,2,1};
+		int lastIndex = buffer.length - 1;
+		int pivot = buffer.length / 2;
+		sorting.sort(buffer,pivot,lastIndex);
+		System.out.println(buffer);
+	}
+
+	public void sort(int Array[],int pivot,int last) {
+		if (pivot < last) {
+			int q = Partition.doPartition(Array,pivot,last);
+			sort(Array,1,Array.length);
+		}
 	}
 }
